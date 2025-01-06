0f7afb0541cbf998d1f21303d0236bde14fde8e0
Le Duc Tho <leductho911@gmail.com>
2025-01-06 18:01:55 +0700

    feat: add log to framework

---
+++ utils/logger.ts
@@ -0,0 +1,32 @@
+import { createLogger, transports, format } from 'winston';
+
+const logger = createLogger({
+  transports: [new transports.Console()],
+  format: format.combine(
+    format.colorize(),
+    format.timestamp(),
+    format.printf(({ timestamp, level, message, service }) => {
+      return `[${timestamp}] ${service} ${level}: ${message}`;
+    })
+  ),
+  defaultMeta: {
+    service: 'NextGen Log -'
+  }
+});
+
+export function printLog(output: string, logLevel = 'info') {
+  switch (logLevel) {
+    case 'debug':
+      logger.debug(output);
+      break;
+    case 'info':
+      logger.info(output);
+      break;
+    case 'warn':
+      logger.warn(output);
+      break;
+    case 'error':
+      logger.error(output);
+      break;
+  }
+}
